301f2c52eb33633f1416490958838e191903fbfa
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-14 23:58:44 +0900

    Trigger build

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const handler = (err, res) => {
     core.setFailed(err.message);
   } else {
     core.setOutput("text", res.text[0]);
+    console.log(`>>>>>>>>>>> Text: ${res.text[0]}`);
   }
 };
 
@@ -13,14 +14,18 @@ async function run() {
   const provider = core.getInput('provider');
   switch (provider) {
     case 'yandex':
+      console.log(`>>>>>>>>>>> yandex chosen`);
       const translate = require('./yandex-provider');
+      console.log(`>>>>>>>>>>> yandex loaded`);
       let source;
       try {
         source = sourceExtract(core.getInput('source'));
+        console.log(`>>>>>>>>>>> source: ${source}`);
       } catch (e) {
         handler(e);
       }
       const resp = await translate(core.getInput('api_key'), source, core.getInput('lang'));
+      console.log(`>>>>>>>>>>> resp: ${resp}`);
       handler(resp);
       break;
     default:
