db63df5485e0bc5b27216a0ed2a86d5b174a0bf9
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-14 23:49:45 +0900

    Add run function

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 const core = require('@actions/core');
 const sourceExtract = require('./source-extractor');
 
-let waiting = true;
-
 const handler = (err, res) => {
   if (err) {
     core.setFailed(err.message);
   } else {
     core.setOutput("text", res.text[0]);
   }
-  waiting = false;
-}; 
+};
 
-const provider = core.getInput('provider');
-switch (provider) {
-  case 'yandex':
-    const translate = require('./yandex-provider');
-    let source;
-    try {
-      source = sourceExtract(core.getInput('source'));
-    } catch (e) {
-      handler(e);
-    }
-    translate(core.getInput('api_key'), source, core.getInput('lang')).then(handler);
-    while (waiting) {      
-    }
-    break;
-  default:
-    handler({ message: `${provider} is not supported` });
-    break;
+async function run() {
+  const provider = core.getInput('provider');
+  switch (provider) {
+    case 'yandex':
+      const translate = require('./yandex-provider');
+      let source;
+      try {
+        source = sourceExtract(core.getInput('source'));
+      } catch (e) {
+        handler(e);
+      }
+      const resp = await translate(core.getInput('api_key'), source, core.getInput('lang'));
+      handler(resp);
+      break;
+    default:
+      handler({ message: `${provider} is not supported` });
+      break;
+  }
 }
+
+run();
