b561be6ec0c262fa284c6f7f4b4c5356856065b5
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-15 00:02:19 +0900

    Fix bug with output

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const handler = (err, res) => {
     core.setFailed(err.message);
   } else {
     core.setOutput("text", res.text[0]);
-    console.log(`>>>>>>>>>>> Text: ${res.text[0]}`);
   }
 };
 
@@ -14,19 +13,15 @@ async function run() {
   const provider = core.getInput('provider');
   switch (provider) {
     case 'yandex':
-      console.log(`>>>>>>>>>>> yandex chosen`);
       const translate = require('./yandex-provider');
-      console.log(`>>>>>>>>>>> yandex loaded`);
       let source;
       try {
         source = sourceExtract(core.getInput('source'));
-        console.log(`>>>>>>>>>>> source: ${source}`);
       } catch (e) {
         handler(e);
       }
       const resp = await translate(core.getInput('api_key'), source, core.getInput('lang'));
-      console.log(`>>>>>>>>>>> resp: ${resp}`);
-      handler(resp);
+      handler(null, resp);
       break;
     default:
       handler({ message: `${provider} is not supported` });
