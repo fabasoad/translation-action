f50bb0c26290ada958739b362bbf5b58a690c697
fabasoad <fabasoad@gmail.com>
2020-10-02 07:50:22 +0900

    Remove Yandex translation tests

--- __tests__/test-providers.js
+++ __tests__/test-providers.js
@@ -8,13 +8,7 @@ const fixture = [{
     name: 'linguatools',
     apiKey: null,
     lang: 'en-de'
-}, /* {
-    title: 'Microsoft',
-    name: 'microsoft',
-    apiKey: process.env.MICROSOFT_API_KEY,
-    addParam: 'canadacentral',
-    lang: 'nl'
-}, */ {
+}, {
     title: 'MyMemory (Free)',
     name: 'mymemory',
     apiKey: null,
@@ -24,26 +18,13 @@ const fixture = [{
     name: 'mymemory',
     apiKey: process.env.MYMEMORY_API_KEY,
     lang: 'en|it'
-}, {
-    title: 'Yandex',
-    name: 'yandex',
-    apiKey: process.env.YANDEX_API_KEY,
-    lang: 'en-tr'
 }];
 
 describe('Provider tests', () => {
     itParam('${value.title} should get correct translation', fixture, async (arg) => {
         const translate = require(`../src/providers/${arg.name}`);
-        try {
-            const translations = await translate(arg.apiKey, 'Evening', arg.lang, arg.addParam);
-            assert(translations.length > 0);
-        } catch ({ message }) {
-            if (message === 'Maximum daily translated text volume exceeded') {
-                console.warn(`[${arg.title}] ${message}`);
-            } else {
-                assert.fail(message);
-            }
-        }
+        const translations = await translate(arg.apiKey, 'Evening', arg.lang, arg.addParam);
+        assert(translations.length > 0);
     });
 
     itParam('${value.title} should fail because of invalid lang', fixture, async (arg) => {
