4cd22280dac1633011db018abc963f2a37ad7b14
fabasoad <fabasoad@gmail.com>
2020-12-20 15:28:57 +0900

    Fix CI error

--- src/index.ts
+++ src/index.ts
@@ -7,40 +7,43 @@ import MyMemoryProvider from './providers/MyMemoryProvider';
 import ProviderBase, { ProviderError } from './providers/ProviderBase';
 import YandexProvider from './providers/YandexProvider';
 
+type ProviderType =
+  'funtranslations' | 'linguatools' | 'microsoft' | 'mymemory' | 'yandex';
+
+const getProvider = (): ProviderBase => {
+  const provider: ProviderType = core.getInput('provider') as ProviderType;
+  switch (provider) {
+  case 'funtranslations':
+    return new FunTranslationsProvider();
+  case 'linguatools':
+    return new LinguaToolsProvider();
+  case 'microsoft':
+    return new MicrosoftProvider(
+      core.getInput('api_key'), core.getInput('api_additional_parameter'));
+  case 'mymemory':
+    return new MyMemoryProvider(core.getInput('api_key'));
+  case 'yandex':
+    return new YandexProvider(core.getInput('api_key'));
+  default:
+    throw new Error(`${provider} is not supported`);
+  }
+}
+
 async function run() {
-  const provider: string = core.getInput('provider');
   try {
     const source: string = extract(core.getInput('source'));
-    let p: ProviderBase;
-    switch (provider) {
-    case 'funtranslations':
-      p = new FunTranslationsProvider();
-    case 'linguatools':
-      p = new LinguaToolsProvider();
-      break;
-    case 'microsoft':
-      p = new MicrosoftProvider(
-        core.getInput('api_key'), core.getInput('api_additional_parameter'));
-      break;
-    case 'mymemory':
-      p = new MyMemoryProvider(core.getInput('api_key'));
-      break;
-    case 'yandex':
-      p = new YandexProvider(core.getInput('api_key'));
-    default:
-      throw new Error(`${provider} is not supported`);
-    }
-    let translation: string;
+    const provider: ProviderBase = getProvider();
+    let text: string;
     try {
-      translation = (await p.translate(source, core.getInput('lang')))[0];
+      text = (await provider.translate(source, core.getInput('lang')))[0];
     } catch (e) {
       if (e instanceof ProviderError) {
-        translation = source;
+        text = source;
       } else {
         throw e;
       }
     }
-    core.setOutput('text', translation);
+    core.setOutput('text', text);
   } catch (e) {
     core.setFailed((<Error>e).message);
   }
