aeab7a73734ada3df0152b8289a4ec8cc05c95e0
fabasoad <fabasoad@gmail.com>
2023-12-30 18:26:32 +0900

    Add wrapper for node-fetch

--- src/providers/wrappers/node-fetch.ts
+++ src/providers/wrappers/node-fetch.ts
@@ -8,5 +8,5 @@ export { RequestInit } from 'node-fetch';
 const fetchPromise: Promise<typeof fetch> = eval('import("node-fetch")')
   .then((mod: { default: typeof fetch }) => mod.default);
 const exportedFetch: typeof fetch =
-  (...args) => fetchPromise.then(fetch => fetch(...args));
+  (...args) => fetchPromise.then((fetch) => fetch(...args));
 export default exportedFetch;
