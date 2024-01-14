c4195c8dd7820bfb387ae77d57994618999fc6c9
fabasoad <fabasoad@gmail.com>
2024-01-14 13:24:07 +0900

    Test 1

--- src/extract.ts
+++ src/extract.ts
@@ -1,6 +1,15 @@
-import fs from 'fs';
+import fs from 'fs'
+import { debug } from '@actions/core'
 
-const extract = (source: string): string =>
-  fs.existsSync(source) ? fs.readFileSync(source, 'utf-8') : source;
+const extract = (source: string): string => {
+  if (fs.existsSync(source)) {
+    debug(`${source} file exists. Reading from file...`)
+    const text = fs.readFileSync(source, 'utf-8')
+    debug(`File content: ${text}`)
+    return text
+  }
+  debug(`${source} file does not exist. "${source}" will be translated`)
+  return source
+}
 
-export default extract;
+export default extract
